2867eb1490b3017188b739e3332e9e0e36f724e8
cstangga <cstangga@naver.com>
2024-07-23 18:14:20 +0900

    feat : index페이지

---
+++ pettopia/src/test/java/com/sh/pettopia/PetTopiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sh.pettopia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTopiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
