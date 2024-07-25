6b3dc448f14e748557ceb29b57f2239f52b6bcd4
cstangga <cstangga@naver.com>
2024-07-25 09:22:10 +0900

    update

---
+++ pettopia/src/main/java/com/sh/pettopia/PettopiaApplication.java
@@ -0,0 +1,13 @@
+package com.sh.pettopia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PettopiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PettopiaApplication.class, args);
+    }
+
+}
