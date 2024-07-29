04e3dd27fa5bdcbe05537dd4811727868e9c4257
cstangga <cstangga@naver.com>
2024-07-29 11:45:20 +0900

    update ncp server db

---
+++ src/main/java/com/sh/pettopia/menu/service/MenuService.java
@@ -0,0 +1,25 @@
+package com.sh.pettopia.menu.service;
+
+
+
+import com.sh.pettopia.menu.entity.Menu;
+import com.sh.pettopia.menu.repository.MenuRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class MenuService {
+    private final MenuRepository menuRepository;
+
+    public ResponseEntity<?> findById(Long menuCode) {
+        Menu menu = menuRepository.findById(menuCode).orElse(null);
+        if(menu == null)
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(menu);
+    }
+}
