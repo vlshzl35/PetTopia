7c789963ae5a5eb9ec518c1be0e0bf53b22fe5c0
youngwoo2 <152594318+youngwoo2@users.noreply.github.com>
2024-07-31 23:14:40 +0900

    Update: 마이페이지 초기설정 (세부항목 폼) #28

--- src/main/java/com/sh/pettopia/enterprise/hospital/controller/HospitalController.java
+++ src/main/java/com/sh/pettopia/enterprise/hospital/controller/HospitalController.java
@@ -1,4 +1,14 @@
 package com.sh.pettopia.enterprise.hospital.controller;
 
+import groovy.util.logging.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
 public class HospitalController {
+    @GetMapping("/map")
+    public void map() {
+
+    }
 }
