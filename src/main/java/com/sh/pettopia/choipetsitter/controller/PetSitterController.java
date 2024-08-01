cdfb3a8c25fb9bc107113fe77fc0b0dbbf6460f8
cstangga <cstangga@naver.com>
2024-08-01 10:20:56 +0900

    update petsitter html

---
+++ src/main/java/com/sh/pettopia/choipetsitter/controller/PetSitterController.java
@@ -0,0 +1,25 @@
+package com.sh.pettopia.choipetsitter.controller;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@org.springframework.stereotype.Controller
+@Slf4j
+@RequestMapping("/petsitter")
+public class PetSitterController {
+
+    @GetMapping("/list")
+    public void list() {
+        // 펫시터 리스트를 가져온다
+    }
+    @GetMapping("/detail")
+    public void detail() {
+        // 펫시터 디테일페이지
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        // 펫시터 홍보글 작성
+    }
+}
