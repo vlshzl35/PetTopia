9f350889ced0cccff6cf5e8584f1709b0da4b381
kishi <81032378+vlshzl35@users.noreply.github.com>
2024-08-01 09:06:12 +0900

    NCP 파일 업로드, 불러오기 수정필요 #30

---
+++ src/main/java/com/sh/pettopia/test/FileDto.java
@@ -0,0 +1,17 @@
+package com.sh.pettopia.test;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Builder
+public class FileDto {
+    private String originalFileName;
+    private String uploadFileName;
+    private String uploadFilePath;
+    private String uploadFileUrl;
+}
