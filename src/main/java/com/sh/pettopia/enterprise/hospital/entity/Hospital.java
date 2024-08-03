73bbc9f274c155100a00280fb1ad804f3aa8c6ab
youngwoo2 <152594318+youngwoo2@users.noreply.github.com>
2024-08-03 16:21:11 +0900

    Update: 엔티티구조 초기설정

--- src/main/java/com/sh/pettopia/enterprise/hospital/entity/Hospital.java
+++ src/main/java/com/sh/pettopia/enterprise/hospital/entity/Hospital.java
@@ -1,4 +1,33 @@
 package com.sh.pettopia.enterprise.hospital.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Table(name = "tbl_hospital")
+@Data
+@Setter(AccessLevel.PRIVATE)
+@NoArgsConstructor
+@AllArgsConstructor
 public class Hospital {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "hospital_id")
+    private Long hospitalId;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "phone")
+    private String phone;
+    @Column(name = "address")
+    private String address;
+    @Column(name = "office_hours")
+    private String officeHours;
+    @Column(name = "detail")
+    private String detail;
+
+    @Column(name = "review_id")
+    private Long reviewId;
+
+    private Hospital hospital;
 }
