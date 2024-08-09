e8b1b25984b8011b4152809b71497852118c7539
Hong-ji-min <jmh907@naver.com>
2024-08-09 21:45:20 +0900

    Feat : Pet등록 Dto  & Controller 추가 #16

--- src/main/java/com/sh/pettopia/Hojji/pet/controller/PetController.java
+++ src/main/java/com/sh/pettopia/Hojji/pet/controller/PetController.java
@@ -1,2 +1,14 @@
-package com.sh.pettopia.Hojji.pet.controller;public class PetController {
+package com.sh.pettopia.Hojji.pet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/pet")
+public class PetController {
+    @GetMapping("/registPet")
+    public void registPet() {
+
+    }
 }
--- src/main/java/com/sh/pettopia/Hojji/pet/dto/PetRegistRequestDto.java
+++ src/main/java/com/sh/pettopia/Hojji/pet/dto/PetRegistRequestDto.java
@@ -1,2 +1,59 @@
-package com.sh.pettopia.Hojji.pet.dto;public class PetRegistRequestDto {
+package com.sh.pettopia.Hojji.pet.dto;
+
+import com.sh.pettopia.Hojji.common.Gender;
+import com.sh.pettopia.Hojji.pet.entity.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Data
+@NotNull
+@AllArgsConstructor
+@Builder
+public class PetRegistRequestDto {
+    // 이름
+    private String name;
+
+    // 성별
+    private PetGender petGender;
+
+    // 견종
+    private String breed;
+
+    // 몸무게
+    private PetSize size;
+
+    // 생일
+    private LocalDate birth;
+
+    // 중성화 여부
+    private boolean neutered;
+
+    // 예방 접종 여부
+    private Set<VaccinationType> vaccinationType;
+
+    // 기생충 예방접종 여부
+    private Set<ParasitePrevention> parasitePrevention;
+
+    //  사회성 및 기타 참고사항
+    private String socialization;
+
+    public Pet toPet() {
+        return Pet.builder()
+                .name(this.name)
+                .petGender(this.petGender)
+                .breed(this.breed)
+                .size(this.size)
+                .birth(this.birth)
+                .neutered(this.neutered)
+                .vaccinationType(this.vaccinationType)
+                .parasitePrevention(this.parasitePrevention)
+                .socialization(this.socialization)
+                .build();
+    }
+
 }
