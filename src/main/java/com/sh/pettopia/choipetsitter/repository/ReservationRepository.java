7fbb3c452d7de1e5bcda4c9c02f1204801146b10
cstangga <cstangga@naver.com>
2024-08-19 14:50:52 +0900

    feat:회원은 결제 가능, 펫시터 예약요청 출력, #144 #49

--- src/main/java/com/sh/pettopia/choipetsitter/repository/ReservationRepository.java
+++ src/main/java/com/sh/pettopia/choipetsitter/repository/ReservationRepository.java
@@ -4,6 +4,11 @@ import com.sh.pettopia.choipetsitter.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+public interface ReservationRepository extends JpaRepository<Reservation,String > {
+    Reservation findByPartnerOrderId(String partnerOrderId);
+
+    List<Reservation> findByPetSitterId(String petSitterId);
 }
--- src/main/java/com/sh/pettopia/kakaopay/dto/KakaoApproveResponse.java
+++ src/main/java/com/sh/pettopia/kakaopay/dto/KakaoApproveResponse.java
@@ -8,19 +8,19 @@ import lombok.ToString;
 @Setter
 @ToString
 public class KakaoApproveResponse {
-    private String aid;
-    private String tid;
-    private String cid;
-    private String sid;
-    private String partner_order_id;
-    private String partner_user_id;
-    private String payment_method_type;
+    private String aid; // 요청 고유 번호
+    private String tid; // 결제 고유 번호
+    private String cid; // 가맹점 코드
+    private String sid; // 정기결제용 ID
+    private String partner_order_id; // 가맹점 주문 번호
+    private String partner_user_id; // 가맹점 회원 id
+    private String payment_method_type; // 결제 수단
 
-    private String item_name;
+    private String item_name; // 아이템 이름, 사이즈 크기 외... 이렇게 써야 겠다
     private String item_code;
-    private Integer quantity;
-    private String created_at;
-    private String approved_at;
+    private Integer quantity; // 총 마리
+    private String created_at; // 결제 요청 시간
+    private String approved_at; // 결제 승인 시간
 
-    private Amount amount;
+    private Amount amount; // 총 금액
 }
