03def6812b21254a68cf729ffca9b728671d304b
cstangga <cstangga@naver.com>
2024-08-31 19:48:17 +0900

    update petsitterController

--- src/main/java/com/sh/pettopia/choipetsitter/controller/PetSitterController.java
+++ src/main/java/com/sh/pettopia/choipetsitter/controller/PetSitterController.java
@@ -124,7 +124,7 @@ public class PetSitterController {
     }
 
     @GetMapping("/detail/{petSitterId}") // 일 시작 게시글 보여주는 핸들러 + 리뷰이미지까지 넣어야 한다
-    public String detail(@PathVariable String petSitterId, Model model) {
+    public String detail(@PathVariable("petSitterId") String petSitterId, Model model) {
         log.info("GET /petsitter/detail/{}", petSitterId);
         PetSitter petSitter = petSitterService.findOneByPetSitter(petSitterId);
         PetSitterRegisterDto dto = new PetSitterRegisterDto().entityToDto(petSitter);
@@ -252,9 +252,9 @@ public class PetSitterController {
             }
         }
 
-        if (!licenseFiles.isEmpty()) {
+        if (!CheckLicenseImageUrl.isEmpty()) {
             //파일들의 url을 List로 담아서 Dto->Entity할 때 넣는다
-            List<FileDto> fileDtoList = fileService.sitterUpFile(licenseFiles, principal.getMember().getEmail(), "licenseImage");
+            List<FileDto> fileDtoList = fileService.sitterUpFile(CheckLicenseImageUrl, principal.getMember().getEmail(), "licenseImage");
             for (FileDto fileDtoImageUrl : fileDtoList) {
                 imageUrl.add(fileDtoImageUrl.getUploadFileUrl());
             }
@@ -365,19 +365,19 @@ public class PetSitterController {
     }
 
     @ResponseBody
-    @PostMapping(value = "/removePostImage")
-    public void removePostImage(String fileNameWithExtension, @AuthenticationPrincipal AuthPrincipal principal) {
+    @PostMapping( "/removePostImage")
+    public void removePostImage(@RequestParam("fileNameWithExtension") String fileNameWithExtension, @AuthenticationPrincipal AuthPrincipal principal) {
         log.info("POST /petsitter/postImage");
         System.out.println("fileNameWithoutExtension = " + fileNameWithExtension);
         fileService.deleteImage(principal.getMember().getEmail(), "imagesInPost", fileNameWithExtension);
     }
 
     @ResponseBody
-    @PostMapping(value = "/removeLicenseImage")
-    public void removeLicenseImage(String fileNameWithExtension, @AuthenticationPrincipal AuthPrincipal principal) {
-        log.info("POST /petsitter/postImage");
-        System.out.println("fileNameWithoutExtension = " + fileNameWithExtension);
-        fileService.deleteImage(principal.getMember().getEmail(), "licenseImage", fileNameWithExtension);
+    @PostMapping( "/removeLicenseImage")
+    public void removeLicenseImage( @RequestParam("licenseImage")String licenseImage, @AuthenticationPrincipal AuthPrincipal principal) {
+        log.info("POST /petsitter/removeLicenseImage");
+        System.out.println("fileNameWithoutExtension = " + licenseImage);
+        fileService.deleteImage(principal.getMember().getEmail(), "licenseImage", licenseImage);
     }
 
     @GetMapping("/petsittingmain")
@@ -422,7 +422,7 @@ public class PetSitterController {
     }
 
     @PostMapping("/reservationOk")
-    public String reservationOk(String partnerOrderId) {
+    public String reservationOk(@RequestParam("partnerOrderId") String partnerOrderId) {
         log.info("POST /petsitter/reservationOk");
 
         Reservation reservation = reservationService.findReservationByPartnerOrderId(partnerOrderId);
@@ -511,7 +511,7 @@ public class PetSitterController {
     }
 
     @PostMapping("/workstatus")
-    public String workStatus(String workStatus,String petSitterId){
+    public String workStatus(@RequestParam("workStatus") String workStatus,@RequestParam("petSitterId")String petSitterId){
         log.info("POST /petsitter/workStatus");
         log.info("workStatus = {}",workStatus);
         log.info("petSitterId = {}",petSitterId);
